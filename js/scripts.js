3df4dd8a1036d53eab594b24c9b7a36f48d32f0b
sharonhalliday <hallidaysharon5@gmail.com>
2023-12-15 12:55:46 -0800

    add inputs

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $("#myForm").submit(function(event) {
+      event.preventDefault();
+        const nameInput = $("input#name").val();
+        const dobInput = $("input#dob").val();
+        const foodInput = $("input#food").val();
+        const musicInput = $("input#music").val();
+        const nounInput = $("input#noun").val();
+        const colorInput = $("input#color").val();
+        const sportInput = $("input#sport").val();
+
+        
+      // $(".name").text(nameInput);
+      // $(".dob").text(dobInput);
+      // $(".food").text(foodInput);
+      // $(".music").text(musicInput);
+      // $(".noun").text(nounInput);
+      // $(".color").text(colorInput);
+      // $(".sport").text(sportInput);
+     
+      // $("#survey").show();
+    });
+});
